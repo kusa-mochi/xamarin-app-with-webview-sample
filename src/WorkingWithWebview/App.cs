11027b9c2d844c20f492d167ea250fa1269b5497
Kusa Mochi <whoatemyapplepie@gmail.com>
2018-09-17 21:32:59 +0900

    make sample code base.

---
+++ src/WorkingWithWebview/App.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace WorkingWithWebview
+{
+	public class App : Application // superclass new in 1.3
+	{
+		public App ()
+		{
+            MainPage = new WebPage { Title = "Web Page" };
+        }
+	}
+}
+
---
+++ src/WorkingWithWebview/WebPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace WorkingWithWebview
+{
+    public class WebPage : ContentPage
+    {
+        public WebPage()
+        {
+            var browser = new WebView();
+            browser.Source = "file:///android_asset/index.html";
+            Content = browser;
+        }
+    }
+}
+
